64355482a0cbc57f3339f909bdf743cb40e98b2e
OT13 <omertahir13@gmail.com>
2021-01-08 07:58:45 +0800

    Added the popup to the main activity

--- app/src/main/java/com/ooteedemo/householdneeds/MainActivity.java
+++ app/src/main/java/com/ooteedemo/householdneeds/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,8 +13,17 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private AlertDialog.Builder builder;
+    private AlertDialog dialog;
+    private Button saveButton;
+    private EditText householdItem;
+    private EditText itemQuantity;
+    private EditText itemColor;
+    private EditText itemSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +36,25 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                createPopupDialog();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
+    }
+
+    private void createPopupDialog() {
+        builder  = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.popup,null);
+        householdItem = view.findViewById(R.id.householdItem);
+        itemQuantity = view.findViewById(R.id.itemQuantity);
+        itemColor = view.findViewById(R.id.itemColor);
+        itemSize = view.findViewById(R.id.itemSize);
+
+        builder.setView(view);
+        dialog = builder.create();
+        dialog.show();
+
     }
 
     @Override
