fd8598673abbdd814f7cd233e683bb4e764c033c
OT13 <omertahir13@gmail.com>
2021-01-19 08:07:05 +0800

    Added the RecyclerViewAdapter (including the ViewHolder inner class)

---
+++ app/src/main/java/com/ooteedemo/householdneeds/ui/RecyclerViewAdapter.java
@@ -0,0 +1,90 @@
+package com.ooteedemo.householdneeds.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+
+import com.ooteedemo.householdneeds.R;
+import com.ooteedemo.householdneeds.model.Item;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    private Context context;
+    private List<Item> itemList;
+
+    public RecyclerViewAdapter(Context context, List<Item> itemList) {
+        this.context = context;
+        this.itemList = itemList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.list_row,viewGroup,false);
+        return new ViewHolder(view, context);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder viewHolder, int position) {
+        Item item = itemList.get(position); // Item object
+        viewHolder.itemName.setText(item.getItemName());
+        viewHolder.itemColor.setText(item.getItemColor());
+        viewHolder.quantity.setText(item.getItemQuantity());
+        viewHolder.size.setText(item.getItemSize());
+        viewHolder.dateAdded.setText(item.getDateItemAdded());
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView itemName;
+        public TextView itemColor;
+        public TextView quantity;
+        public TextView size;
+        public TextView dateAdded;
+        public Button editButton;
+        public Button deleteButton;
+        public int id;
+
+        public ViewHolder(@NonNull View itemView, Context ctx) {
+            super(itemView);
+            context = ctx;
+
+            itemName = itemView.findViewById(R.id.householdItem);
+            itemColor = itemView.findViewById(R.id.itemColor);
+            quantity = itemView.findViewById(R.id.itemQuantity);
+            size = itemView.findViewById(R.id.itemSize);
+            dateAdded = itemView.findViewById(R.id.item_date);
+
+            editButton = itemView.findViewById(R.id.edit_button);
+            deleteButton = itemView.findViewById(R.id.delete_button);
+            editButton.setOnClickListener(this);
+            editButton.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.edit_button:
+//                    do edit item stuff
+                    break;
+                case R.id.delete_button:
+//                    do delete item stuff
+                    break;
+            }
+        }
+    }
+}
